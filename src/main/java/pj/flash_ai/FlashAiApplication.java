a7ffa6d1eadc1ae4c972996f2f7beb5db10d2e13
svinstvo-og <maxim12.05.2006koval@gmail.com>
2024-10-08 21:32:07 +0200

    First commit

---
+++ src/main/java/pj/flash_ai/FlashAiApplication.java
@@ -0,0 +1,13 @@
+package pj.flash_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pj/flash_ai/FlashAiApplicationTests.java
@@ -0,0 +1,13 @@
+package pj.flash_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
